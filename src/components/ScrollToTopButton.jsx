17a3ff2e73cf04aee4aa39d42b3f1df9779259f4
aijoyzuo <cctaijoy@gmail.com>
2025-11-13 23:57:33 +0800

    說明這次改了什麼

--- src/components/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton.jsx
@@ -3,17 +3,17 @@ import React, { useEffect, useState } from 'react';
 const ScrollToTopButton = () => {
   const [isVisible, setIsVisible] = useState(false);
 
-  // 監聽滾動事件
+
   useEffect(() => {
     const handleScroll = () => {
-      setIsVisible(window.scrollY > 200); // 捲動超過 200px 顯示
+      setIsVisible(window.scrollY > 200); 
     };
 
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  // 點擊回到頂部
+  
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
--- src/pages/Home/sections/Portfolio.jsx
+++ src/pages/Home/sections/Portfolio.jsx
@@ -1,4 +1,17 @@
-export default function Portfolio(){
-    return(<>
+export default function Portfolio() {
+    return (<>
+        <div className="row">
+            <div className="col-4">
+                hola mongo
+            </div>
+            <div className="col-8">
+                <div className="image-scroll-container row flex-nowrap overflow-auto">
+                    <div className="col-8 col-md-4"><img src={process.env.PUBLIC_URL + '/images/home/work1-1.png'} className="img-fluid" /></div>
+                    <div className="col-8 col-md-4"><img src={process.env.PUBLIC_URL + '/images/home/work1-2.png'} className="img-fluid" /></div>
+                    <div className="col-8 col-md-4"><img src={process.env.PUBLIC_URL + '/images/home/work1-3.png'} className="img-fluid" /></div>
+                </div>
+            </div>
+        </div>
+
     </>)
 }
